2965359c58cd75f9341b671c0a8082b105735c12
VermiIIi0n <104612478+VermiIIi0n@users.noreply.github.com>
2022-11-23 21:33:54 +0800

    Update .gitignore

--- Rust/target/debug/deps/vermils-c1044a8d4637a506.d
+++
@@ -1,5 +0,0 @@
-/Users/ayasa/Resilio Sync/uninet/VermilsMagicPocket/Rust/target/debug/deps/vermils-c1044a8d4637a506.rmeta: src/main.rs
-
-/Users/ayasa/Resilio Sync/uninet/VermilsMagicPocket/Rust/target/debug/deps/vermils-c1044a8d4637a506.d: src/main.rs
-
-src/main.rs:
--- Rust/target/debug/deps/vermils-cc049e6a2ee151c3.d
+++
@@ -1,5 +0,0 @@
-/Users/ayasa/Resilio Sync/uninet/VermilsMagicPocket/Rust/target/debug/deps/vermils-cc049e6a2ee151c3.rmeta: src/main.rs
-
-/Users/ayasa/Resilio Sync/uninet/VermilsMagicPocket/Rust/target/debug/deps/vermils-cc049e6a2ee151c3.d: src/main.rs
-
-src/main.rs:
